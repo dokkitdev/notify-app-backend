3fa935452bb1970e0a454a50ba4d54c98af6c47e
OmenWD <omenpars@gmail.com>
2018-09-04 14:59:34 +0300

    [add] AwsServiceProvider

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,11 +50,19 @@
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
                             @endif
+
+
                         @else
+                            @if(Auth::user()->role=='admin')
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/admin/index">Admin panel</a>
+                                </li>
+                            @endif
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
+
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
