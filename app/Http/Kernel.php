af7c50004889e5b73871eb299988ad175936492d
OmenWD <omenpars@gmail.com>
2018-09-10 10:00:39 +0300

    [edit] import Customers workers

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -59,7 +59,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'checkAdmin' => \App\Http\Middleware\checkAdmin::class,
-        'checkLogin' => \App\Http\Middleware\checkLogin::class,
+        'CheckAdmin' => \App\Http\Middleware\checkAdmin::class,
+        'CheckLogin' => \App\Http\Middleware\checkLogin::class,
     ];
 }
