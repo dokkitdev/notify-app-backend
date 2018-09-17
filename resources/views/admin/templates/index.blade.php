19a860ecdf44eee0c1f83494323ba0a2c97539ed
OmenWD <omenpars@gmail.com>
2018-09-17 10:41:24 +0300

    [edit] edit templates index blade

--- resources/views/admin/templates/index.blade.php
+++ resources/views/admin/templates/index.blade.php
@@ -1,41 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-
     <div class="container">
-
         <h1>Templates</h1>
+        <h2 class="pt-3">Private</h2>
         @foreach ($templatesGroups as $tg)
             <div class="content">
-                <h3>{{$tg->customer_group_tag}}</h3>
-                <div class="border">
-                    <table class="table">
-                        @foreach ($tg['templates'] as $template)
-                        <tr>
-                            <td>Letter {{$template->state}}</td>
-                            <td>
-                                {{$terms[$template->term]}}
-                            </td>
-                            <td>
-                                @if($template->name=='')<a class="btn btn-danger" href="/admin/template/{{$template->id}}" title="Create template"><i class="fas fa-plus"></i></a>@endif
-                            </td>
-                            <td>
-                                @if($template->name!='')<a class="btn btn-primary" href="/admin/template/{{$template->id}}/edit" title="Edit template"><i class="fas fa-pen"></i></a>@endif
-                            </td>
-                            <td>
-                                @if($template->name!='')<a class="btn btn-dark" href="/admin/template/{{$template->id}}/email" title="Email to Me"><i class="far fa-envelope"></i></a>@endif
-                            </td>
-                        </tr>
-                        @endforeach
-                    </table>
-                </div>
-
+                <table class="table table-bordered">
+                    @foreach ($tg['templates'] as $template)
+                    <tr>
+                        <td>Letter {{$template->state}}</td>
+                        <td>
+                            {{$terms[$template->term]}}
+                        </td>
+                        <td>
+                            @if($template->name=='')<a class="btn btn-danger" href="/admin/template/{{$template->id}}" title="Create template"><i class="fas fa-plus"></i></a>@endif
+                            @if($template->name!='')<a class="btn btn-primary" href="/admin/template/{{$template->id}}/edit" title="Edit template"><i class="fas fa-pen"></i></a>@endif
+                            @if($template->name!='')<a class="btn btn-dark" href="/admin/template/{{$template->id}}/email" title="Email to Me"><i class="far fa-envelope"></i></a>@endif
+                        </td>
+                    </tr>
+                    @endforeach
+                </table>
             </div>
         @endforeach
-
     </div>
+    <div class="clearfix"></div>
     <div class="container">
-        <h2>Housing Authorities</h2>
+        <h2 class="pt-5">Housing Authorities</h2>
         <div class="content">
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
@@ -48,31 +39,24 @@
             {!! Form::submit('Add new', ['class'=>'btn btn-primary float-right mb-3']) !!}
         </div>
     </div>
+    <div class="clearfix"></div>
     <div class="container">
     @foreach ($housingTemplatesGroups as $tg)
         <div class="content">
-            <h3>{{$tg->company_name}}</h3>
-            <div class="border">
-                <table class="table">
-                    @foreach ($tg['housingTemplates'] as $housingTemplate)
-                        <tr>
-                            <td>Letter {{$housingTemplate->state}}</td>
-                            <td>
-                                @if($housingTemplate->name=='')<a class="btn btn-danger" href="/admin/housingtemplate/{{$template->id}}" title="Create template"><i class="fas fa-plus"></i></a>@endif
-                            </td>
-                            <td>
-                                @if($housingTemplate->name!='')<a class="btn btn-primary" href="/admin/housingtemplate/{{$template->id}}/edit" title="Edit template"><i class="fas fa-pen"></i></a>@endif
-                            </td>
-                            <td>
-                                @if($housingTemplate->name!='')<a class="btn btn-dark" href="/admin/housingtemplate/{{$template->id}}/email" title="Email to Me"><i class="far fa-envelope"></i></a>@endif
-                            </td>
-                        </tr>
-                    @endforeach
-                </table>
-            </div>
-
+        <h3>{{$tg->company_name}}</h3>
+            <table class="table table-bordered">
+                @foreach ($tg['housingTemplates'] as $housingTemplate)
+                    <tr>
+                        <td>Letter {{$housingTemplate->state}}</td>
+                        <td>
+                            @if($housingTemplate->name=='')<a class="btn btn-danger" href="/admin/housingtemplate/{{$template->id}}" title="Create template"><i class="fas fa-plus"></i></a>@endif
+                            @if($housingTemplate->name!='')<a class="btn btn-dark" href="/admin/housingtemplate/{{$template->id}}/email" title="Email to Me"><i class="far fa-envelope"></i></a>@endif
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
         </div>
     @endforeach
     </div>
-
+    <div class="clearfix"></div>
 @endsection
