36e6fe6f7f158f24c8f14d89d69266c04838b0fd
OmenWD <omenpars@gmail.com>
2018-09-19 12:16:27 +0300

    [add] data for templates replace Service

---
+++ app/Service/templateDataService.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Service;
+
+use App\Customers;
+use App\SimProContracts;
+use App\SimProJobs;
+
+class templateDataService
+{
+    public function makeTemplateDataPrivate(SimProContracts $contract){
+        $sps=new simProService();
+
+        $customer=Customers::find($contract->customers_id);
+        $customerParsedData=json_decode($customer->parsedData);
+        $contractParsedData=json_decode($contract->parsedData);
+        //dd($customerParsedData);
+        $siteData=$sps->getSiteData($customer->company_id,23967); #todo надо забрать из списка сайтов
+
+        $data=[];
+        $data['customer_address']=$customer->address;
+        $data['date_now']=date('d M Y',time());
+        $data['name']=$customerParsedData->Title.' '.$customerParsedData->FamilyName;
+        //$data['site_address']=$siteData->Address->Address;
+        $data['site_assets']='ERROR';
+        $data['contract_tab']='ERROR';
+        $data['from_name']='ERROR';
+        $data['client_address']='ERROR';
+        $data['contract_ref']=$contractParsedData->ContractNo;
+        $data['customer_ref']=$customerParsedData->ID;
+        $data['date_expired']=date("d/m/Y",strtotime($contractParsedData->EndDate));
+        return $data;
+    }
+    public function makeTemplateDataHousing(SimProJobs $job){
+        $sps=new simProService();
+        $customer=Customers::where('simpro_id','=',$job->simpro_customer_id)->first();
+        $customerParsedData=json_decode($customer->parsedData);
+        $jobParsedData=json_decode($job->parsedData);
+        $siteData=$sps->getSiteData($customer->company_id,$jobParsedData->Site->ID);
+        $data=[];
+        $data['customer_address']=$customer->address;
+        $data['site_address']=$siteData->Address->Address;
+        $data['site_primary_contact']=$siteData->PrimaryContact->Title.' '.$siteData->PrimaryContact->FamilyName;
+        $data['job_number']=$jobParsedData->ID;
+        $data['site_name']=$siteData->Name;
+        $data['date_now']=date('d M Y',time());
+        return $data;
+    }
+}
