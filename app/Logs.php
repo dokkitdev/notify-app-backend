f78995d2b2b379944100134e75da1b4ff2630c0b
YOUR_USERNAME <vitaliy.roslov@aily.team>
2018-11-26 13:15:38 +0200

    logs

--- app/Logs.php
+++ app/Logs.php
@@ -9,5 +9,10 @@ class Logs extends Model
     protected $table = 'logs';
     protected $fillable = ['customer_type', 'letters_generated', 'email_generated', 'pdf'];
 
-
+    public function getCreatedAt()
+    {
+        $date = $this->created_at;
+        $date = $date ? \DateTime::createFromFormat('Y-m-d H:i:s', $date) : null;
+        return $date ? $date->format('Y-m-d') : '';
+    }
 }
