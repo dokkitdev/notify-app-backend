cc9def1feb096a84693bbded9691e1c2e52532b2
YOUR_USERNAME <vitaliy.roslov@aily.team>
2018-12-04 14:50:13 +0200

    s

--- app/Console/Commands/PrivateCommand.php
+++ app/Console/Commands/PrivateCommand.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Service\PrivateUploader;
 use App\Service\simProRequestService;
+use App\Service\Upload\PrivateUpload;
 use Illuminate\Console\Command;
 
 class PrivateCommand extends Command
@@ -19,43 +20,43 @@ class PrivateCommand extends Command
 
     public function handle()
     {
-        (new PrivateUploader())
-            ->start();
+        (new PrivateUpload())
+            ->run();
     }
 
-    public function q()
-    {
-        $sim = new simProRequestService();
-        $companies = $sim->getRequest('GET', '/api/v1.0/companies/0/customers/companies/');
-        foreach ($companies as $companyId) {
-            $company = $sim->getRequest('GET', '/api/v1.0/companies/0/customers/companies/' . $companyId->ID);
-
-            $contracts = $sim->getRequest('GET', '/api/v1.0/companies/0/customers/' . $companyId->ID . '/contracts/');
-            $contracts_id = [];
-            foreach ($contracts as $contractId) {
-                $contract = $sim->getRequest('GET', '/api/v1.0/companies/0/customers/' . $companyId->ID . '/contracts/' . $contractId->ID);
-                $contracts_id[] = $contractId->ID;
-            }
-            if (sizeOf($contracts) < 1) {
-                continue;
-            }
-
-            //create customer
-            //create contracts
-
-            if (isset($company->Sites)) {
-                foreach ($company->Sites as $siteId) {
-                    $assets = $sim->getRequest('GET', '/api/v1.0/companies/0/sites/' . $siteId->ID . '/assets/?CustomerContract ne()');
-                    foreach ($assets as $assetId) {
-                        $asset = $sim->getRequest('GET', '/api/v1.0/companies/0/sites/' . $siteId->ID . '/assets/' . $assetId->ID);
-                        if (sizeOf($asset->CustomerContract) < 1) {
-                            continue;
-                        }
-                    }
-                }
-            }
-
-        }
-        die;
-    }
+//    public function q()
+//    {
+//        $sim = new simProRequestService();
+//        $companies = $sim->getRequest('GET', '/api/v1.0/companies/0/customers/companies/');
+//        foreach ($companies as $companyId) {
+//            $company = $sim->getRequest('GET', '/api/v1.0/companies/0/customers/companies/' . $companyId->ID);
+//
+//            $contracts = $sim->getRequest('GET', '/api/v1.0/companies/0/customers/' . $companyId->ID . '/contracts/');
+//            $contracts_id = [];
+//            foreach ($contracts as $contractId) {
+//                $contract = $sim->getRequest('GET', '/api/v1.0/companies/0/customers/' . $companyId->ID . '/contracts/' . $contractId->ID);
+//                $contracts_id[] = $contractId->ID;
+//            }
+//            if (sizeOf($contracts) < 1) {
+//                continue;
+//            }
+//
+//            //create customer
+//            //create contracts
+//
+//            if (isset($company->Sites)) {
+//                foreach ($company->Sites as $siteId) {
+//                    $assets = $sim->getRequest('GET', '/api/v1.0/companies/0/sites/' . $siteId->ID . '/assets/?CustomerContract ne()');
+//                    foreach ($assets as $assetId) {
+//                        $asset = $sim->getRequest('GET', '/api/v1.0/companies/0/sites/' . $siteId->ID . '/assets/' . $assetId->ID);
+//                        if (sizeOf($asset->CustomerContract) < 1) {
+//                            continue;
+//                        }
+//                    }
+//                }
+//            }
+//
+//        }
+//        die;
+//    }
 }
