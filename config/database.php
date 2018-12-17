712e04bc3209e0f9acdb9ce4c036c6828c0408f2
YOUR_USERNAME <vitaliy.roslov@aily.team>
2018-12-17 15:55:09 +0200

    s

--- config/database.php
+++ config/database.php
@@ -5,7 +5,7 @@ $host = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $database = substr($url["path"], 1);
-return [  
+return [   
 
     /*
     |--------------------------------------------------------------------------
