925ab3e12b8eef7dd52315383133c60fc17bb4a7
YOUR_USERNAME <vitaliy.roslov@aily.team>
2019-01-01 21:05:48 +0200

    some changes

--- app/Http/Controllers/Profile.php
+++ app/Http/Controllers/Profile.php
@@ -6,20 +6,37 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Redirect;
 
 class Profile extends Controller
 {
-    public function getProfile()
+    public function getProfile(Request $request)
     {
-        $title='Profile';
-        return view('auth.profile', ['user'=>User::find(Auth::id())])->with('title',$title);
+
+        $title = 'Profile';
+        return view('auth.profile', [
+            'user' => User::find(Auth::id()),
+        ])->with('title', $title);
     }
-    public function saveProfile(Request $request){
-        $user=User::find(Auth::id());
-        $data=$request->all();
+
+    public function saveProfile(Request $request)
+    {
+        $data = $request->all();
+        $newPass = $data['newpass'];
+        $repeatPass = $data['repeatpass'];
+        if ($newPass !== $repeatPass) {
+            return redirect()->back()->with([
+                'newPass' => $newPass,
+                'repeatPass' => $repeatPass,
+                'error' => 'Password are not same',
+            ]);
+        }
+        $user = User::find(Auth::id());
         //$user->name=$data['name'];
-        $user->password=Hash::make($data['newpass']);
+        $user->password = Hash::make($newPass);
         $user->save();
-        return redirect('/profile');
+        return redirect()->back()->with([
+            'ok' => 'You are successfully change password',
+        ]);
     }
 }
