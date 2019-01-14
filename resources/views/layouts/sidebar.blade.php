536e67723cb6d977bf3f6c9ec0f9f86bbc43cba3
YOUR_USERNAME <vitaliy.roslov@aily.team>
2019-01-14 10:55:31 +0200

    private

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -7,8 +7,8 @@
         </a>
     </li>
     <li class="nav-item {!! Route::current()->getName() == 'private.all' ? 'active' : '' !!}">
-        <a class="nav-link" href="#">
-            {{--{{ route('private.all') }}--}}
+        <a class="nav-link" href="{{ route('private.all') }}">
+            {{----}}
             <i class="fas fa-fw fa-folder"></i>
             <span>Private Contracts</span>
         </a>
