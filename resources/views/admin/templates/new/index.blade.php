e7190b143886a2c950deac63b0e5a184bd7a0357
Vitaliy Roslov <vitaliy.s.roslov@gmail.com>
2020-06-04 17:00:53 +0300

    accordion

--- resources/views/admin/templates/new/index.blade.php
+++ resources/views/admin/templates/new/index.blade.php
@@ -8,41 +8,55 @@
 @endsection
 @section('content')
     <h1>Templates</h1>
-    @foreach ($templateParents as $parent)
-        <h4 class="pt-3">{!! $parent->title !!}</h4>
-        <div class="content">
-            <table class="table table-bordered" style="table-layout: fixed;">
-                @foreach ($parent->templates as $t)
-                    <tr>
-                        <td>{!! $t->title !!} {!! $t->tag !!}</td>
-                        {{--@if ($t->term !== null)--}}
-                            {{--<td>{!! $t->term !!} {!! $t->term > 1 ? 'weeks' : 'week' !!}</td>--}}
-                        {{--@endif--}}
-                        <td>
-                            <form>
-                                @csrf
-                                {{--@if ($t->is_html)--}}
-                                    {{--<a class="btn btn-primary" href="{{ route('templates.edit', ['id' => $t->id]) }}"--}}
-                                       {{--title="Edit template"><i class="fas fa-pen"></i></a>--}}
-                                {{--@endif--}}
-                                <input type="file" name="file" class="d-none" accept=".docx"/>
-                                <input type="hidden" name="alias" value="{!! $t->alias !!}">
-                                <a href="#" style="margin: 0 20px;" class="upload-file"><i class="fas fa-upload"></i>
-                                    Upload</a>
-                                <a download @if ($t->docx)
-                                href="/storage/docx/{!! $t->docx !!}" class='download'
-                                   @else
-                                   href="#" class='download disabled'
-                                        @endif
-                                ><i class="fas fa-download"></i> Download</a>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            </table>
-        </div>
-        <div class="clearfix"></div>
-    @endforeach
+    <div class="accordion" id="myAccordion">
+        @foreach ($templateParents as $key => $parent)
+            <div class="card">
+                <div class="card-header" id="accordion-{!! $key !!}">
+                    <h2 class="mb-0">
+                        <button type="button" class="btn btn-link" data-toggle="collapse"
+                                data-target="#accordion-collapse-{!! $key !!}">{!! $parent->title !!}</button>
+                    </h2>
+                </div>
+                <div id="accordion-collapse-{!! $key !!}" class="collapse" aria-labelledby="accordion-{!! $key !!}"
+                     data-parent="#myAccordion">
+                    <div class="card-body">
+                        <table class="table table-bordered" style="table-layout: fixed;">
+                            @foreach ($parent->templates as $t)
+                                <tr>
+                                    <td>{!! $t->title !!} {!! $t->tag !!}</td>
+                                    {{--@if ($t->term !== null)--}}
+                                        {{--<td>{!! $t->term !!} {!! $t->term > 1 ? 'weeks' : 'week' !!}</td>--}}
+                                    {{--@endif--}}
+                                    <td>
+                                        <form>
+                                            @csrf
+                                            {{--@if ($t->is_html)--}}
+                                                {{--<a class="btn btn-primary"--}}
+                                                   {{--href="{{ route('templates.edit', ['id' => $t->id]) }}"--}}
+                                                   {{--title="Edit template"><i class="fas fa-pen"></i></a>--}}
+                                            {{--@endif--}}
+                                            <input type="file" name="file" class="d-none" accept=".docx"/>
+                                            <input type="hidden" name="alias" value="{!! $t->alias !!}">
+                                            <a href="#" style="margin: 0 20px;" class="upload-file"><i
+                                                        class="fas fa-upload"></i>
+                                                Upload</a>
+                                            <a download @if ($t->docx)
+                                            href="/storage/docx/{!! $t->docx !!}" class='download'
+                                               @else
+                                               href="#" class='download disabled'
+                                                    @endif
+                                            ><i class="fas fa-download"></i> Download</a>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        @endforeach
+    </div>
 @endsection
 @section('js')
     <script src="{{ asset('js/jquery.toaster.js') }}"></script>
