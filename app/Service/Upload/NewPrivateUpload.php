9bc2e7c6f314f3b1e09d0a3269400b43fb1b4a95
root <vitaliy.s.roslov@gmail.com>
2020-12-14 20:55:55 +0000

    s

--- app/Service/Upload/NewPrivateUpload.php
+++ app/Service/Upload/NewPrivateUpload.php
@@ -262,7 +262,7 @@ class NewPrivateUpload
 
         $recurringInvoiceSections = $this->simpro->getRequest(
             'get',
-            '/api/v1.0/companies/0/recurringInvoices/'.$recurringInvoiceId.'/sections/'
+            '/api/v1.0/companies/0/recurringInvoices/'.$recurringInvoiceId.'/sections/?pageSize=100'
         );
         if ($recurringInvoiceSections) {
             foreach ($recurringInvoiceSections as $recurringInvoiceSection) {
