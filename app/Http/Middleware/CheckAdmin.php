8096abd155326c732ffaaaf0886114d37e20e3d8
Vitalii Roslov <vitalii.r@flash-web.net>
2022-08-03 11:50:16 +0300

    report

--- app/Http/Middleware/CheckAdmin.php
+++ app/Http/Middleware/CheckAdmin.php
@@ -23,16 +23,3 @@ class checkAdmin
         return $next($request);
     }
 }
-
-
-    class A {
-        static function call() {
-            echo 'a';
-        }
-    }
-
-    class B extends A{
-
-    }
-
-    B::call();
