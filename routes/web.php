6881dc19beca18093a581989b393dbae2a58210a
Vitalii Roslov <vitalii.r@flash-web.net>
2022-09-01 20:46:00 +0300

    logout

--- routes/web.php
+++ routes/web.php
@@ -134,7 +134,7 @@ Route::group(
 //Auth::routes();
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
 if (env('ALLOW_REGISTRATION') == true) {
 // Registration Routes...
