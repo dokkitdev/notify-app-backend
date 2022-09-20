fee972e63b3cb2f923532b6b33ead159cbccc743
Vitalii Roslov <vitalii.r@flash-web.net>
2022-09-21 02:27:50 +0300

    predis

--- config/database.php
+++ config/database.php
@@ -111,7 +111,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
